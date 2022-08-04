15b1e235e7db71c1bdb14dcb77382408d36ad434
tejashchaudhari <109734065+tejashchaudhari@users.noreply.github.com>
2022-08-04 12:01:44 +0530

    Add files via upload

---
+++ ASSIGNMENT/Module-3/Module-1/information.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    printf("my name is: tejash chaudhari");
+    printf("\n my birth date is: 27/10/1998");
+    printf("\n my age is: 23 years old");
+    printf("\n my address is: at.ekalkham, po.kuilivel, ta. songadh, dist. tapi");
+    getch();
+
+}
