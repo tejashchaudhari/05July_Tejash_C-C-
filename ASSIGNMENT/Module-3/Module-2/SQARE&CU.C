f37c5160e88e1100179b8f7c92d2720b32da6b01
tejashchaudhari <109734065+tejashchaudhari@users.noreply.github.com>
2022-08-04 12:09:52 +0530

    Add files via upload

---
+++ ASSIGNMENT/Module-3/Module-2/SQARE&CU.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+   int n;
+   clrscr();
+   printf("Enter a sqare & cube number:");
+   scanf("%d", &n);
+   printf("\nsqare of the number %d is %d", n, n*n);
+   printf("\ncube of the number %d is %d", n, n*n*n);
+   getch();
+
+}
